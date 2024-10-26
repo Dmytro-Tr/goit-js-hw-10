3a1c062a270a1854d54c9584d2cb4e285d030c53
Dmtro_Tr <Di4gamegem@gmail.com>
2024-10-26 21:59:09 +0300

    complate timer

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,55 @@
-console.log('Snackbar');
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputNumber = document.querySelector('input[type=number]');
+
+addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delay = event.target.elements.delay.value;
+  console.log(delay);
+  const state = event.target.elements.state.value;
+  console.log(state);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+
+        iziToast.show({
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          titleSize: '16px',
+          messageColor: '#fff',
+          backgroundColor: '#59a10d',
+          position: 'topRight',
+          timeout: '3000',
+          closeOnClick: 'true',
+          progressBarColor: '#fff',
+          transitionIn: 'bounceInDown',
+          transitionOut: 'flipOutX',
+        });
+      } else {
+        reject(delay);
+
+        iziToast.show({
+          message: `❌ Rejected promise in ${delay}ms`,
+          titleSize: '16px',
+          messageColor: '#fff',
+          backgroundColor: '#ef4040',
+          position: 'topRight',
+          timeout: '3000',
+          closeOnClick: 'true',
+          progressBarColor: '#fff',
+          transitionIn: 'bounceInDown',
+          transitionOut: 'flipOutX',
+        });
+      }
+    }, delay);
+    inputNumber.value = '';
+  });
+}
